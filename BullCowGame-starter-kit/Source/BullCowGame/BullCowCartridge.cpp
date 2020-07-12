64c760b7e66cdb9369f6bdc2c9e9a72ebe3431ea
포도맛 버섯 <skacjstk0818@gmail.com>
2020-07-12 22:02:41 +0900

    [2020.07.12] udemy 언리얼 49 50 기본 게임루프

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,13 +4,26 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-	PrintLine(TEXT("Hello PrintLine\nSecond Line"));
+	PrintLine(TEXT("Hello PrintLine\n"));
+	PrintLine(TEXT("Guess the N letter word! ")); //Magin Number Remove!
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
 	ClearScreen();
-	FString HiddenWord = TEXT("cake");
+	FString HiddenWord = TEXT("cake");	//Move outside this function 
+	//PrintLine(Input);
+
+	int32 HwLen = HiddenWord.Len();
+	FString FW = FString::FromInt(HwLen);
+	//PrintLine(FW);	//단어 숫자 출력
+	if (Input.Equals(HiddenWord)) { // == 도 되는거같음
+		PrintLine(TEXT("You have Won!"));	
+	}
+	else
+	{
+		PrintLine(TEXT("You have Lost!"));
+	}
 	
 
 } 
