a0d59322cdd479d9e0e999758f34a4d632a83a50
포도맛 버섯 <skacjstk0818@gmail.com>
2020-07-22 23:46:34 +0900

    [2020.07.22] udemy 언리얼 59 Boolean

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -6,36 +6,33 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     Super::BeginPlay();
 	//welcoming player
 	SetUpGame();//Setting up Game
-
-	PrintLine(FString::Printf(TEXT("The HiddenWord is: %s\n and %i long "), *HiddenWord,HiddenWord.Len())); //Debug Line
-	
-	//	PrintLine(FW);	//단어 숫자 출력
-	PrintLine(TEXT("Welcome to BullCow Game"));
-	//PrintLine(TEXT("Guess the "+ FW +" letter word! ")); //Magin Number Remove!
-	//TEXT 는 FString 밖에 안들어감. Magin Number는 하드코딩된 숫자에 그렇게 쓰더라구
-
-	PrintLine(TEXT("Guess the %i letter word! "), HiddenWord.Len());
-	//FString::Printf() 없이도 위의 문장이 실행됨. debug line에서 보다시피
-	//원래는 FString::Printf 를 통해 % 형식 지정자를 사용할 수 있는데
-	 //Prompt Player For Guess
+	//PrintLine(FString::Printf(TEXT("The HiddenWord is: %s\n and %i long "), *HiddenWord,HiddenWord.Len())); //Debug Line
+	//SetUpGame() 에 통합.
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-	ClearScreen();
-
-	//Checking PlayerGuess
-	//if(Input == HiddenWord)// == 도 되는거같음
-	if (Input.Equals(HiddenWord)) { 
-		PrintLine(TEXT("You have Won!"));	
+	if (bGameOver) {	//게임오버 라면
+		ClearScreen();
+		SetUpGame();
 	}
-	else
-	{
-		if (Input.Len() != HiddenWord.Len()) {
-			PrintLine(TEXT("The HiddenWord is %i charactor long, try again."),HiddenWord.Len());
+	else {
+		//Checking PlayerGuess
+		//if(Input == HiddenWord)// == 도 되는거같음
+		if (Input.Equals(HiddenWord)) {
+			PrintLine(TEXT("You have Won!"));
+			EndGame();
 		}
-		PrintLine(TEXT("You have Lost!"));
+		else
+		{
+			if (Input.Len() != HiddenWord.Len()) {
+				PrintLine(TEXT("The HiddenWord is %i charactor long, try again."), HiddenWord.Len());
+			}
+				PrintLine(TEXT("You have Lost!"));
+				EndGame();
+		}
 	}
+
 
 	//Check If Isogram
 	//Prompt To guess again	
@@ -56,5 +53,19 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 void UBullCowCartridge::SetUpGame() {
 	HiddenWord = TEXT("cakes");	
 	lives = 4;	//typedef 로 지정해서 int32는 int랑 같은데
-	
+	bGameOver = false;
+
+	//	PrintLine(FW);	//단어 숫자 출력
+	PrintLine(TEXT("Welcome to BullCow Game"));
+	//PrintLine(TEXT("Guess the "+ FW +" letter word! ")); //Magin Number Remove!
+	//TEXT 는 FString 밖에 안들어감. Magin Number는 하드코딩된 숫자에 그렇게 쓰더라구
+
+	PrintLine(TEXT("Guess the %i letter word! "), HiddenWord.Len());
+	//FString::Printf() 없이도 위의 문장이 실행됨. debug line에서 보다시피
+	//원래는 FString::Printf 를 통해 % 형식 지정자를 사용할 수 있는데
+	 //Prompt Player For Guess
+}
+void UBullCowCartridge::EndGame() {
+	PrintLine(TEXT("Press enter to play again"));	
+	bGameOver = true;
 }
