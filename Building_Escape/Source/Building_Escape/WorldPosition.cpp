fc75f63bf1168edc58cc081d4b981d3c623708e1
포도맛 버섯 <skacjstk0818@gmail.com>
2020-08-29 23:28:12 +0900

    [2020.08.29] udemy 언리얼 94 Actor's Transform GetActor...

--- Building_Escape/Source/Building_Escape/WorldPosition.cpp
+++ Building_Escape/Source/Building_Escape/WorldPosition.cpp
@@ -2,7 +2,7 @@
 
  
 #include "WorldPosition.h"
-	#include "GameFramework/Actor.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -20,25 +20,15 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	//FString log = TEXT("Hello");
-	//FString* ptrLog = &log;
-
-	//log.Len();
-
-	//(*ptrLog).Len();
-	//ptrLog->Len();
-	////precedence 우선순위
-	//UE_LOG(LogTemp, Warning, TEXT("%s"), **ptrLog);
-
-
+	//200829  udemy 언리얼 94 
 	FString objectName = GetOwner()->GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s"), *objectName);
-
-
-	//LogTemp 카테고리에 3가지 Verbosity, 그리고 메시지
-//	UE_LOG(LogTemp, Display, TEXT("Just log, Hello !")); //회색
-//	UE_LOG(LogTemp, Warning, TEXT("This is a Warning")); //노란색
-//	UE_LOG(LogTemp, Error, TEXT("This is a Error"));	//빨간색 
+	FString objectPosition = GetOwner()->GetActorLocation().ToString();
+	FString objectPositionTransform = GetOwner()->GetActorTransform().GetLocation().ToString();
+	//xyz 위치
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *objectPosition);
+	//xyz 위치 회전 스케일  (GetLoaction 추가시 위와 같음)
+	UE_LOG(LogTemp, Warning, TEXT("trans %s"), *objectPositionTransform);
 }
 
 
@@ -50,3 +40,27 @@ void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 	// ...
 }
 
+
+/*
+	//(*ptrLog).Len();
+	//ptrLog->Len();
+	////precedence 우선순위
+
+	//udemy 언리얼 91 start
+	//LogTemp 카테고리에 3가지 Verbosity, 그리고 메시지
+//	UE_LOG(LogTemp, Display, TEXT("Just log, Hello !")); //회색
+//	UE_LOG(LogTemp, Warning, TEXT("This is a Warning")); //노란색
+//	UE_LOG(LogTemp, Error, TEXT("This is a Error"));	//빨간색
+	//udemy 언리얼 91 end
+
+	//200829  udemy 언리얼 94 start
+	FString objectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *objectName);
+	FString objectPosition = GetOwner()->GetActorLocation().ToString();
+	FString objectPositionTransform = GetOwner()->GetActorTransform().GetLocation().ToString();
+	//xyz 위치
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *objectPosition);
+	//xyz 위치 회전 스케일  (GetLoaction 추가시 위와 같음)
+	UE_LOG(LogTemp, Warning, TEXT("trans %s"), *objectPositionTransform);
+	//200829  udemy 언리얼 94 end
+*/
