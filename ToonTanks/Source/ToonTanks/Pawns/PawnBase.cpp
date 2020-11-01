76d368f1c5ea40828168b9b9094962039dc5f4f2
포도맛 버섯 <skacjstk0818@gmail.com>
2020-11-01 20:32:04 +0900

    [2020.11.01] udemy 언리얼 137 Deriving Blueprint Classes

--- ToonTanks/Source/ToonTanks/Pawns/PawnBase.cpp
+++ ToonTanks/Source/ToonTanks/Pawns/PawnBase.cpp
@@ -4,7 +4,7 @@
 #include "PawnBase.h"
 #include "Components/CapsuleComponent.h"
 
-// Sets default values
+// Sets default values 
 APawnBase::APawnBase()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
