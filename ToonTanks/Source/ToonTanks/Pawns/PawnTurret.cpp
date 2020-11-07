e36d9807dcc51a9b306bfe5d92340b40591a1402
포도맛 버섯 <skacjstk0818@gmail.com>
2020-11-07 18:40:27 +0900

    [2020.11.07] udemy 언리얼 143 PawnTurret

---
+++ ToonTanks/Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnTurret.h"
+#include "Kismet/GameplayStatics.h"
+APawnTurret::APawnTurret() {
+
+}
+
+void APawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+	GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+
+	//	GetWorldTimerManager()	같음.
+}
+
+// Called every frame
+void APawnTurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+void APawnTurret::CheckFireCondition() 
+{
+	// if player == null || is dead then bail
+
+	// If player 가 사거리 내로 왔을 때 FIRE
+
+	UE_LOG(LogTemp, Warning, TEXT("Fire Condition Checked!"));
+}
