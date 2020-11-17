a9a8613c2032d2d29e1a1e82f1eb303c8cfe8b05
포도맛 버섯 <skacjstk0818@gmail.com>
2020-11-17 23:30:15 +0900

    [2020.11.17] udemy 언리얼 151 Creating  HealthComponent

---
+++ ToonTanks/Source/ToonTanks/Components/HealthComponent.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+class ATankGameModeBase;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TOONTANKS_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+private:
+
+	UPROPERTY(EditAnyWhere)
+	float DefaultHealth = 100.0f;
+	float Health = 0.0f;
+
+	ATankGameModeBase* GameModeRef;
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void TakeDamage(AActor* DamageActor, float Damage, const UDamageType* DamageType, AController* InstigateBy, AActor* DamageCauser);
+
+
+		
+};
